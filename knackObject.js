cb590234797cca308823ec37ff5c8f426e59cc04
jason-0 <jason.0.employment@gmail.com>
2015-10-28 01:59:00 -0700

    further debugging global KnackObject

--- knackObject.js
+++ knackObject.js
@@ -17,7 +17,7 @@
 
 $( document ).on( resume.eventTrigger, resume.render );
  */
-function KnackObject(options) { 'use strict';
+window.KnackObject = function(options) { 'use strict';
 
   /**
    * creates a new object, based on the defaults extended by options.
@@ -65,7 +65,7 @@ function KnackObject(options) { 'use strict';
     this.settings.hideSpinner.call();
 
   }
-}
+};
 
 KnackObject.prototype = {
 
@@ -105,7 +105,7 @@ KnackObject.prototype = {
     'hideSpinner'  : Knack.hideSpinner()
   },
 
-    /**
+  /**
      * recursivly coalates a knack object to show the field labels and the records
      * if the record data is a connection (relation), then coalate its data too
      * @requires  JQuery
@@ -114,194 +114,192 @@ KnackObject.prototype = {
      * @param  {object|undefined} fieldnames @optional precomputing the fieldnames saves ajax requests
      * @return {object}                      the coalated object
      */
-    get : function ( objectId, recordId, fieldnames ) {
+  get : function ( objectId, recordId, fieldnames ) {
 
-      var buffer = [];
-      var tempRecord = [];
-      var childFieldNames = [];
+    var buffer = [];
+    var tempRecord = [];
+    var childFieldNames = [];
 
-      // get default values for passed variables if they are undefined
-      if ( typeof objectId === 'undefined' ) {
-        objectId = this.settings.objectId;
-      }
+    // get default values for passed variables if they are undefined
+    if ( typeof objectId === 'undefined' ) {
+      objectId = this.settings.objectId;
+    }
 
-      if ( typeof recordId === 'undefined') {
-        recordId = this.settings.recordId;
-      }
+    if ( typeof recordId === 'undefined') {
+      recordId = this.settings.recordId;
+    }
 
-      // Get the field names for the object's fields if not provided
-      // this saves pummeling the Knack API more than necessary
-      if ( typeof fieldnames === 'undefined' ) {
-        $.getJSON(
-          'https://api.knackhq.com/v1/objects/' + objectId + '/fields',
-          buffer = response.fields
-        );
-      } else {
-        buffer = fieldnames;
-      }
-
-      // get the specific data for the object fields
+    // Get the field names for the object's fields if not provided
+    // this saves pummeling the Knack API more than necessary
+    if ( typeof fieldnames === 'undefined' ) {
       $.getJSON(
-        'https://api.knackhq.com/v1/objects/' + objectId + '/records/' + recordId,
-        function(response) {
-          tempRecord = response;
-        }
+        'https://api.knackhq.com/v1/objects/' + objectId + '/fields',
+        buffer = response.fields
       );
+    } else {
+      buffer = fieldnames;
+    }
 
-      //loop through each field in the object
-      for ( var x = 0, l = buffer.length; x < l; x++ ) {
-
-        //if this field is not a connection (relational)
-        if (buffer[x].type !== 'connection' ) {
-
-          //then just store the server compiled html and raw data
-          buffer[x].html = tempRecord[ buffer[x].key ];
-          buffer[x].raw  = tempRecord[ buffer[x].key + '_raw' ];
-
-        //otherwise this is a relational field (connection)
-        } else {
-
-          //create the connection array in this buffer element
-          buffer[x].connection = [];
-
-          //get the labels for the related records
-          $.getJSON(
-            'https://api.knackhq.com/v1/objects/' + buffer[x].relationship.object + '/fields',
-            function(response) {
-              childFieldNames = response.fields;
-            }
-          );
-
-          //loop through each connection's records
-          tempRecord[ buffer[x].key +'_raw' ].forEach( function( child, index ) {
-
-            //recursivly cooalate the connection's records
-            buffer[x].connection[index]            = {};
-            buffer[x].connection[index].records    = {};
-            buffer[x].connection[index].records    = this.get( buffer[x].relationship.object, child.id, childFieldNames);
-            buffer[x].connection[index].id         = child.id;
-            buffer[x].connection[index].identifier = child.identifier;
-          } );
-        }
+    // get the specific data for the object fields
+    $.getJSON(
+      'https://api.knackhq.com/v1/objects/' + objectId + '/records/' + recordId,
+      function(response) {
+        tempRecord = response;
       }
+    );
 
-      if ( 
-        this.settings.debug && 
-        typeof fieldnames === 'undefined'
+    //loop through each field in the object
+    for ( var x = 0, l = buffer.length; x < l; x++ ) {
+
+      //if this field is not a connection (relational)
+      if (buffer[x].type !== 'connection' ) {
+
+        //then just store the server compiled html and raw data
+        buffer[x].html = tempRecord[ buffer[x].key ];
+        buffer[x].raw  = tempRecord[ buffer[x].key + '_raw' ];
+
+      //otherwise this is a relational field (connection)
+      } else {
+
+        //create the connection array in this buffer element
+        buffer[x].connection = [];
+
+        //get the labels for the related records
+        $.getJSON(
+          'https://api.knackhq.com/v1/objects/' + buffer[x].relationship.object + '/fields',
+          function(response) {
+            childFieldNames = response.fields;
+          }
+        );
+
+        //loop through each connection's records
+        tempRecord[ buffer[x].key +'_raw' ].forEach( function( child, index ) {
+
+          //recursivly cooalate the connection's records
+          buffer[x].connection[index]            = {};
+          buffer[x].connection[index].records    = {};
+          buffer[x].connection[index].records    = this.get( buffer[x].relationship.object, child.id, childFieldNames);
+          buffer[x].connection[index].id         = child.id;
+          buffer[x].connection[index].identifier = child.identifier;
+        } );
+      }
+    }
+
+    if ( 
+      this.settings.debug && 
+      typeof fieldnames === 'undefined'
+    ) {
+      console.log( 'Knack Object:' );
+      console.log( buffer );
+    }
+    return buffer;
+  },
+
+  /**
+   * recursively creates a string of HTML that represents the Knack object
+   * @param  {array|object}          objects  array of knack objects
+   * @param  {boolean|undefined}     level    @optional set to true if recursing
+   * @return {string}                         HTML representing Knack object
+   */
+  template : function ( objects, recursionLevel ) {
+
+    var
+      _buffer = {},
+      x,
+      l;
+
+    // get coallated object if object not passed as variable
+    if ( typeof objects === 'undefined') {
+      objects = this.get();
+    } 
+
+    // if objects is an array of objects,
+    if ( objects.isArray() ) {
+
+    // check for a special case
+    // if there are 3 connection and they are labeled "Identifier", "Title" and "Details"
+    // then render
+      if (
+        objects.connection.length   === 3 &&
+        objects.connection[0].label === 'Identifier' &&
+        objects.connection[1].label === 'Title' &&
+        objects.connection[2].label === 'Detail'
       ) {
-        console.log( 'Knack Object:' );
-        console.log( buffer );
-      }
-      return buffer;
-    },
+        _buffer += '<div class="knackObjectLabel">' + objects.connection[1].html + '</div>' +
+          '<div class="knackObjectDetails">' + objects.connection[1].html + '</div>';
 
-    /**
-     * recursively creates a string of HTML that represents the Knack object
-     * @param  {array|object}          objects  array of knack objects
-     * @param  {boolean|undefined}     level    @optional set to true if recursing
-     * @return {string}                         HTML representing Knack object
-     */
-    template : function ( objects, recursionLevel ) {
+      // otherwise recurse through array of relational connection or records arrays
+      } else {
+        for ( x = 0, l = objects.length; x < l; x++ ) {
 
-      var
-        _buffer = {},
-        x,
-        l;
-
-      // get coallated object if object not passed as variable
-      if ( typeof objects === 'undefined') {
-        objects = this.get();
-      } 
-
-      // if objects is an array of objects,
-      if ( objects.isArray() ) {
-
-      // check for a special case
-      // if there are 3 connection and they are labeled "Identifier", "Title" and "Details"
-      // then render
-        if (
-          objects.connection.length   === 3 &&
-          objects.connection[0].label === 'Identifier' &&
-          objects.connection[1].label === 'Title' &&
-          objects.connection[2].label === 'Detail'
-        ) {
-          _buffer += '<div class="knackObjectLabel">' + objects.connection[1].html + '</div>' +
-            '<div class="knackObjectDetails">' + objects.connection[1].html + '</div>';
-
-        // otherwise recurse through array of relational connection or records arrays
-        } else {
-          for ( x = 0, l = objects.length; x < l; x++ ) {
-
-            // only recurse through records not in settings.templateSkipRecord
-            if ( ! ( object[x].label in this.settings.templateSkipRecord ) ) {
-              _buffer += this.template( objects[x], level );
-            }
+          // only recurse through records not in settings.templateSkipRecord
+          if ( ! ( object[x].label in this.settings.templateSkipRecord ) ) {
+            _buffer += this.template( objects[x], level );
           }
         }
-
-      // otherwise if the objects variable is an object,
-
-      // check if objects variable is relational,(links to other objects)
-      } else if ( objects.type === 'connection' ) {
-
-        // create header based on level of relationship,
-        if ( typeof recursionLevel === 'undefined' ) {
-          _buffer += '<h3>' + objects.label + '</h3>';
-        }
-
-        // and recurse through objects connection
-        for ( x = 0, l=  objects.connection.length; x < l; x++ ) {
-          _buffer += template( objects.connection[x], 1 );
-        }
-
-      // otherwise, check if the objects variable has a "records" key
-      // if it does, then we are inside a relational child
-      // so recurse through the array of records
-      } else if ( typeof objects.records !== 'undefined' ) {
-          for ( x = 0, l=  objects.records.length; x < l; x++ ) {
-          _buffer += template( objects.records[x], 1 );
-        }
-
-      // otherwise objects is not relational, add ojects' label and html details to template
-      } else {
-        _buffer += '<div><span class="knackObjectLabel">' + objects.label + '</span>' +
-        '<span class="knackObjectDetails'> + objects.html + '</span></div>';
-      }
-      return _buffer;
-    },
-
-    /**
-     * renders an html string into a Knack view
-     *
-     * @param  {string}            htmlString ...erm, this is a string of HTML
-     * @param  {string|undefined}  elementId   @optional the HTML element ID to append the HTML to
-     *                                         if no elementID is passed, then append an
-     *                                         appropriatly wrapped element into the Knack view
-     * @return {undefined}         
-     */
-    render : function ( htmlString, elementId ) {
-      var $element = {};
-
-      if ( typeof htmlString === 'undefined') {
-        htmlString = this.template();
       }
 
-      if ( typeof elementId === 'undefined' ) {
-        elementId = this.settings.elementId;
+    // otherwise if the objects variable is an object,
+
+    // check if objects variable is relational,(links to other objects)
+    } else if ( objects.type === 'connection' ) {
+
+      // create header based on level of relationship,
+      if ( typeof recursionLevel === 'undefined' ) {
+        _buffer += '<h3>' + objects.label + '</h3>';
       }
 
-      $element = $( '#' + elementId );
-
-      if ( $element.length ) {
-        $element.append( htmlString );
-      } else {
-        $('.kn_scenes:first').append( '<div class="kn_scene"><div class="kn_view">' + htmlString + '</div></div>' );
+      // and recurse through objects connection
+      for ( x = 0, l=  objects.connection.length; x < l; x++ ) {
+        _buffer += template( objects.connection[x], 1 );
       }
-    },
 
-    eventTrigger : function () {
-      return 'knack-page-render.'+ this.settings. viewId;
+    // otherwise, check if the objects variable has a "records" key
+    // if it does, then we are inside a relational child
+    // so recurse through the array of records
+    } else if ( typeof objects.records !== 'undefined' ) {
+        for ( x = 0, l=  objects.records.length; x < l; x++ ) {
+        _buffer += template( objects.records[x], 1 );
+      }
+
+    // otherwise objects is not relational, add ojects' label and html details to template
+    } else {
+      _buffer += '<div><span class="knackObjectLabel">' + objects.label + '</span>' +
+      '<span class="knackObjectDetails'> + objects.html + '</span></div>';
     }
-  };
+    return _buffer;
+  },
 
- window.KnackObject = KnackObject;
+  /**
+   * renders an html string into a Knack view
+   *
+   * @param  {string}            htmlString ...erm, this is a string of HTML
+   * @param  {string|undefined}  elementId   @optional the HTML element ID to append the HTML to
+   *                                         if no elementID is passed, then append an
+   *                                         appropriatly wrapped element into the Knack view
+   * @return {undefined}         
+   */
+  render : function ( htmlString, elementId ) {
+    var $element = {};
+
+    if ( typeof htmlString === 'undefined') {
+      htmlString = this.template();
+    }
+
+    if ( typeof elementId === 'undefined' ) {
+      elementId = this.settings.elementId;
+    }
+
+    $element = $( '#' + elementId );
+
+    if ( $element.length ) {
+      $element.append( htmlString );
+    } else {
+      $('.kn_scenes:first').append( '<div class="kn_scene"><div class="kn_view">' + htmlString + '</div></div>' );
+    }
+  },
+
+  eventTrigger : function () {
+    return 'knack-page-render.'+ this.settings. viewId;
+  }
+};
